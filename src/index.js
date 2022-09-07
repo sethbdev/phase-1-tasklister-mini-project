29ff7154d9885561f79c87b60cb9f60c6872269b
Seth <seth.bbrendle@gmail.com>
2022-09-07 07:51:59 -0400

    changed master to main, added buttons to todo's, can now delete todo's

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const submitTarget = document.getElementById(['new-task-description']);
+    createToDo(e.target['new-task-description'].value);
+    form.reset();
+  })
 });
+
+function createToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = 'x';
+  btn.addEventListener('click', deleteToDo);
+  li.textContent = `${todo}  `;
+  li.appendChild(btn);
+  document.getElementById('tasks').appendChild(li)
+
+}
+
+function deleteToDo(e) {
+  e.target.parentNode.remove();
+}
